e282a6954250339c796f779bd94051ef0522392e
Elamaran Shanmugam <elamaras@amazon.com>
2020-11-05 22:19:29 -0500

    first commit

---
+++ deploy-dash.sh
@@ -0,0 +1,22 @@
+mkdir -p $DIR/_output
+CLOUDWATCH_NAMESPACE="AppMeshExample/gateway-envoy/StatsD"
+MESH_NAME=yelb
+VIRTUAL_NODE_NAME=yelb-app-server
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+PROJECT_NAME="$(basename ${DIR})"
+
+eval "cat <<EOF
+$(<${DIR}/cw-dashboard.yaml.template)
+EOF
+" >$DIR/_output/${VIRTUAL_NODE_NAME}-cw-dashboard.yaml
+
+        echo "Deploying stack ${VIRTUAL_NODE_NAME}-cw-dashboard, this may take a few minutes..."
+        aws cloudformation deploy \
+            --no-fail-on-empty-changeset \
+            --stack-name ${PROJECT_NAME}-${VIRTUAL_NODE_NAME} \
+            --template-file "$DIR/_output/$VIRTUAL_NODE_NAME-cw-dashboard.yaml" \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides \
+            "DashboardName=${PROJECT_NAME}-${VIRTUAL_NODE_NAME}"
+
+
